1a15283bd94b460dc282ea5bff61fff0b92f9868
chenghaolun <1194234765@qq.com>
2019-03-04 10:30:36 +0800

    create server,create mongoose

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const http = require('http')
+const Account = require('./model')
+
+const server = http.createServer(function(request, response) {
+  if (request.url === '/') {
+    response.writeHead(200, {
+      "Content-Type": "text/html"
+    })
+
+    response.end('Hello node')
+  }
+})
+
+server.listen(3000)
 
---
+++ model/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/user', {useNewUrlParser: true})
+
+module.exports = mongoose.model('User', new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String
+  }
+}))
